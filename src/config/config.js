160297b3442e182e18e553a1eb23e4791ee2f188
sdfunes <sdfunes@gmail.com>
2025-09-12 10:18:32 -0300

    feat: agrego mi propia mongodb

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
   PORT: 3000,
   MONGO_URL:
-    'mongodb+srv://coder:codercoder@cluster0.olj5ktk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  DB_NAME: 'clase08comis76495',
-  SECRET: 'password123',
+    'mongodb+srv://sdfunes:b2ttTHds38CpMRU5@coderbackend1.hyvmwfb.mongodb.net/?retryWrites=true&w=majority&appName=coderbackend1',
+  DB_NAME: 'ecommerce',
+  SECRET: 'b2ttTHds38CpMRU5',
 };

987252ef6faac33966c7ddb6cd4aae76de566426
sdfunes <sdfunes@gmail.com>
2025-09-13 12:27:50 -0300

    fix: saco credenciales para la entrega

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
   PORT: 3000,
   MONGO_URL:
-    'mongodb+srv://sdfunes:b2ttTHds38CpMRU5@coderbackend1.hyvmwfb.mongodb.net/?retryWrites=true&w=majority&appName=coderbackend1',
+    'mongodb+srv://user:secretKey@coderbackend1.hyvmwfb.mongodb.net/?retryWrites=true&w=majority&appName=coderbackend1',
   DB_NAME: 'ecommerce',
-  SECRET: 'b2ttTHds38CpMRU5',
+  SECRET: 'secretKey',
 };
